02392829ba44f4ae6e4fbd4e6045b33a1b122262
Pallibhaskararao <pallibhaskararao21@gmail.com>
2025-07-04 12:26:07 +0530

    Basic input is created to take the urls

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function handleShorten() {
+  setError("");
+  if (!longUrl.trim()) {
+    setError("Please enter a URL.");
+    return;
+  }
+  if (!isValidUrl(longUrl)) {
+    setError("Invalid URL format.");
+    return;
+  }
+}
+
+export default function App() {
+  const [longUrl, setLongUrl] = useState("")
+
+  const SaveUrl = () =>{
+    setUrl(document.getElementsByClassName("url").value)
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+        <h1>URL shorter Apllication </h1>
+        <h3>Enter the link to convert into short link : </h3>
+        <input type = "url" 
+            className = "url" 
+            placeholder="Enter the LongUrl to convert " 
+            value={longUrl}
+          onChange={(e) => setLongUrl(e.target.value)}
+            />
+        <div className="card">
+          <button className='generate' onClick={handleShorten}>
+            Generate
+          </button>
+        </div>
+
+      {/* {url.map((l,index) =>(
+        <li key = {index} >
+          {l}
+        </li>
+      ))} */}
+
+      <footer style={{ marginTop: 40, textAlign: "center", color: "#666" }}>
+        Made with in React — Fully Client Side
+      </footer>
+    </div>
     </>
   )
 }
 
-export default App
+
+
